8ee50e051fadd5201623989f11755ababfcdacd1
p-lacasaost <p-lacasaost@izzi.mx>
2025-06-16 06:56:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 cnx = st.connection('snowflake')
 session= cnx.session()
 
@@ -31,6 +32,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list :
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -44,8 +47,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-#New section
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
